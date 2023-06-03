61978d7af5d2d42cedeab1b812f3a0352cc1eb78
João Artur Santos <joaoartursantos25@gmail.com>
2023-06-03 10:16:13 +0100

    new files

---
+++ js/moves.js
@@ -0,0 +1,32 @@
+// Selecionar a imagem com a classe "clickMe"
+const clickImg = document.querySelector(".clickMe");
+const logo = document.querySelector(".logotipo")
+
+// Selecionar a imagem com a classe "nome"
+const nomeImg = document.querySelector(".nome");
+
+// Selecionar o id do audio
+const myAudio = document.querySelector('#myAudio');
+const myPop = document.querySelector('#myPop');
+
+clickImg.addEventListener("click", () => {
+
+    myPop.play();
+    clickImg.classList.add('slide-right');
+    setTimeout(() => {
+        clickImg.style.opacity = "0";
+          }, 2000);
+    setTimeout(() => {
+    nomeImg.style.display = "flex";
+    clickImg.style.display = "none";
+      }, 3000);
+    setTimeout(() => {
+    myAudio.play();
+    nomeImg.style.opacity = "1"; 
+
+    setTimeout(() => {
+      window.location.href = "/Users/joaoartursantos/Desktop/OneDrive/Documentos/PROGRAMAÇÃO/LeWagon/index.html";
+  }, 4100);
+}, 4100);
+});
+
